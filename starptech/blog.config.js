678d9008b99006df997bc36127383c334d35f585
Dustin Deus <deusdustin@gmail.com>
2020-05-21 23:39:42 +0200

    migrate blog to next.js

---
+++ starptech/blog.config.js
@@ -0,0 +1,31 @@
+
+const BLOG = {
+  author: 'Dustin Deus',
+  title: 'Dustin Deus',
+  description: 'Hi, my name is Dustin and I’m Fullstack Software Engineer from Germany',
+  summary: 'From Ø to Hero, Web and Distributed Systems.',
+  language: 'en',
+  latestLimit: 5,
+
+  labels: {
+    default: 'Home',
+    latest: 'Latest',
+    list: 'All',
+  },
+
+  email: 'deusdustin@gmail.com',
+  github: 'starptech',
+  twitter: 'dustindeus',
+  domain: 'starptech.de',
+  
+  googleAnalytics: 'UA-115030952-1',
+  cn: false,
+  enableViews: true,
+  
+  layouts: {
+    pageWidth: '750px',
+    pageWidthMobile: '88vw',
+  },
+}
+
+export default BLOG
